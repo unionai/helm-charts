a53ac4bb460538eb5d130c27902bf7998c947847
Chris Soyars <chris@union.ai>
2025-03-12 10:00:37 -0700

    chore(aws-example): switch to api-based eks auth

--- providers/aws/provider.tf
+++ providers/aws/provider.tf
@@ -28,21 +28,3 @@ provider "helm" {
     }
   }
 }
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.default.token
-  # exec {
-  #   api_version = "client.authentication.k8s.io/v1beta1"
-  #   args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  #   command     = "aws"
-  # }
-}
-
-# provider "kubectl" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.default.token
-#   load_config_file       = false
-# }
