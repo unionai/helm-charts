e898213ce4198c8b6438b35b61ff5e98e80cb717
Chris Soyars <chris@union.ai>
2025-03-17 08:43:54 -0700

    fix(terraform-aws): use larger default instance type, helm token (#56)

--- providers/aws/provider.tf
+++ providers/aws/provider.tf
@@ -18,8 +18,8 @@ provider "aws" {
 
 provider "helm" {
   kubernetes {
-    host = module.eks.cluster_endpoint
-    # token                  = data.aws_eks_cluster_auth.default.token
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.default.token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
