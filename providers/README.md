ab6de00227aacffa70a5f14b1ccb8a2704748bb6
Rob Lyon <47508711+ctxswitch@users.noreply.github.com>
2025-03-24 14:51:23 -0700

    ctx/chore(providers): remove the example cloud provider configs (#77)
    * [x] Pull the example cloud provider examples back into our internal repository in favor of documenting the requirements.

--- providers/README.md
+++
@@ -1,3 +0,0 @@
-# Sample Terraform Configuration
-
-Provides examples of the minimal infrastructure setup for running the Union operator.
--- providers/azure/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  name_prefix       = var.name_prefix
-  normalized_prefix = lower(replace("${local.name_prefix}", "/[^a-zA-Z0-9]/", ""))
-}
